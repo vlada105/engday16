73ae338b43a1102fb72e0d3ee107394c82ff2be4
ChenateXs <nikolacenak@gmail.com>
2022-02-24 11:15:09 +0100

    Implementation of GuitarSong()

--- day16-project/src/main/Main.java
+++ day16-project/src/main/Main.java
@@ -1,5 +1,11 @@
 package main;
 
-public class Main {
+import model.Entity;
+import model.GuitarSong;
 
+public class Main {
+	public static void main(String[] args) {
+		Entity g = new GuitarSong(260);
+		System.out.println(g.sayHello());
+	}
 }
