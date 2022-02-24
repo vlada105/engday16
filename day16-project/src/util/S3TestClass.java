2e6368766435b2463a679afae75ddbc336b3f5db
vlada105 <vlaja105@gmail.com>
2022-02-24 16:36:36 +0100

    SourcetreeTest Comment

---
+++ day16-project/src/util/S3TestClass.java
@@ -0,0 +1,19 @@
+package util;
+
+public class S3TestClass {
+	
+	private final int num;
+
+	public S3TestClass(int num) {
+		super();
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+	
+	
+	
+
+}
