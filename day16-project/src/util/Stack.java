ec7769fe4eb5f52d90db6e12de1119c29de8ce17
Milos Milic <mmilic@gmail.com>
2022-02-24 13:00:36 +0100

    Stack definition

---
+++ day16-project/src/util/Stack.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.util.List;
+
+public class Stack {
+
+	private final List<Integer> data;
+
+	public Stack(List<Integer> data) {
+		super();
+		this.data = data;
+	}
+
+	public int sum() {
+		throw new RuntimeException("Not yet implemented!");
+	}
+
+}
